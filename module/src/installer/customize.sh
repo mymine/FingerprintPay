a2e737bc45adff30ec62d145ef2113e489bb14b1
jason <eritpchy@qq.com>
2025-03-29 18:05:13 +0800

    Workaround for issue #222 3

--- module/src/installer/customize.sh
+++ module/src/installer/customize.sh
@@ -2,8 +2,18 @@ ui_print "Fingerprint Pay Batch Installer"
 ls "$MODPATH"
 ui_print "$ZIPFILE"
 INSTALLER_MODPATH="$MODPATH"
+INSTALLER_MODID="$MODID"
 for ZIPFILE in $MODPATH/*-release.zip; do
     ui_print "$ZIPFILE"
     install_module
 done
-rm -rf "$INSTALLER_MODPATH/" || true
+if [ "$KSU" ]; then
+  # KernelSU Next 我不想知道为什么一定要module.prop，否则报错
+  (sleep 1 && rm -rf "$INSTALLER_MODPATH/")&
+  # 如果安装器不慎被安装，直接删掉
+  # 直接删也是不行的，发癫
+  (sleep 1 && [ -d "$NVBASE/modules/$INSTALLER_MODID/" ] && rm -rf "$NVBASE/modules/$INSTALLER_MODID/")&
+else
+  rm -rf "$INSTALLER_MODPATH/"
+fi
+ui_print "- Finish"
